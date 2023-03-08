103a563f4a0997533ab1b31dbb39a7d10fc9b959
MonicaAuditore <mauditore@live.it>
2023-03-08 15:33:05 +0100

    add_header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,57 @@
-<h1>Hello World</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- link cdn bootstrap -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/css/bootstrap.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
+  <!-- fine bootstrap -->
+  <title>Laravel Primi Passi</title>
+</head>
+<body>
+  <!-- nav -->
+<nav class="navbar navbar-expand-lg bg-body-tertiary navbar-light ">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Logo</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Destinations</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<!-- header -->
+<header class="bg-info" style="--bs-bg-opacity: .1;">
+  <div class="container">
+    <div class="row" style="padding: 200px 0px;">
+      <div class="col" >
+        <h1>TRAVEL AGENCY</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo tenetur consequuntur aliquam tempora porro! Reiciendis, perspiciatis ratione.</p>
+        <button type="button" class="btn btn-primary mt-4">Discover more</button>
+      </div>
+      <div class="col ">
+        <!-- <img src="../img/4027 [Convertito].png"> -->
+      </div>
+    </div>
+    
+  </div>
+</header>
+</body>
+</html>
+
